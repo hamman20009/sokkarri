7b08087210dc4cec28af56ef03128c4b46a947d6
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-09-16 05:34:01 +0200

    Update products.js

--- products.js
+++ products.js
@@ -4,7 +4,7 @@ const products = [
 		logo:'img/logo.png',
 		image: 'img/1.png',
 		
-		title: 'العلاج المثالي لمرضضى السكري        '  ,
+		title: 'العلاج المثالي لمرضى السكري'  ,
     '  ,
 		price1: 6500,
 		price: 3000,
